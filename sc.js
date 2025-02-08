deaeb88e27f30d289a2455ee9182bdd6263e07e9
yh102756 <cvr101049@gmail.com>
2025-02-09 01:44:49 +0900

    Update sc.js

--- sc.js
+++ sc.js
@@ -176,7 +176,7 @@ function showAll() {// 목록 전부 보여주기
     showTable(title);
     htmlText += "<br><table align=\"center\" <caption><h3 id=\"trot\">트로트</h3></caption>";
     showTable(trot);
-    htmlText += "<br><table align=\"center\" <caption><h3 id=\"old\">옛 가요</h3></caption>";
+    htmlText += "<br><table align=\"center\" <caption><h3 id=\"old\">팝송</h3></caption>";
     showTable(old);
     htmlText += "<br><table align=\"center\" <caption><h3 id=\"other\">팝, 애니</h3></caption>";
     showTable(other);
